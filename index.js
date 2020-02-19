b0239bf400f3492867affdfe7cdd278a45c76372
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-19 20:47:56 +0300

    Handling Movie Selection

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
 
+  if (!movies.length) {
+    dropdown.classList.remove('is-active');
+    return;
+  }
+
   resultsWrapper.innerHTML = '';
   dropdown.classList.add('is-active');
 
@@ -43,8 +48,18 @@ const onInput = async event => {
       ${movie.Title}
     `;
 
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+    });
+
     resultsWrapper.appendChild(option);
   }
 };
 
 input.addEventListener('input', debounce(onInput));
+document.addEventListener('click', event => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active');
+  }
+});
