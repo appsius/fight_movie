8e6e88e11e1f3d474d072be297a6b429dc935ba6
apsius <38158216+appsius@users.noreply.github.com>
2020-02-23 10:11:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: '8e5c5dc5',
+      apikey: '',
       i: movie.imdbID
     }
   });
